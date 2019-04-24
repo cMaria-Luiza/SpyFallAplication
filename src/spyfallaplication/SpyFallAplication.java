957d37c3b0e21fd25d897b240418d2639714c46f
cMaria-Luiza <43617346+cMaria-Luiza@users.noreply.github.com>
2019-04-24 08:34:22 -0600

    Update SpyFallAplication.java
    atualizacao

--- src/spyfallaplication/SpyFallAplication.java
+++ src/spyfallaplication/SpyFallAplication.java
@@ -13,96 +13,26 @@ public class SpyFallAplication {
     
     private ArrayList<Carta> cartas = new ArrayList<>(MAX_JOGADORES);
     private static ArrayList<Jogador> jogadores = new ArrayList<>(MAX_JOGADORES);
-    private List<List<String>> lugares = new ArrayList<>(5);
+    private ArrayList<String> lugares = new ArrayList<String>(5);
     private int espiaoID;
     private static String lugarDaPartida;
-    private static int IDlugar;
+    private static int lugarID;
     
     private final int HOSPITAL = 0;
     private final int RESTAURANTE = 1;
     private final int ESCOLA = 2;
     private final int CONCERTO_MUSICAL = 3;
-    private final int PARQUE = 4;
+    private final int PRACA = 4;
     
     public SpyFallAplication() {
-        // cria um arraylist de arraylists
-        for(int i = 0; i < 5; i++)  
-            lugares.add(new ArrayList<>(3));
-            
-        // inicializa o arraylist de lugares
         setLugares();
-        //sortearLugar();
         
         
     }
     
-    public void setLugares() {
-        lugares.get(HOSPITAL).add("Médica");
-        lugares.get(HOSPITAL).add("Paciente");
-        lugares.get(HOSPITAL).add("Visitante");
-        
-        lugares.get(RESTAURANTE).add("Garçon");
-        lugares.get(RESTAURANTE).add("Cliente");
-        lugares.get(RESTAURANTE).add("Faxineiro");
-        
-        lugares.get(ESCOLA).add("Diretor");
-        lugares.get(ESCOLA).add("Aluno");
-        lugares.get(ESCOLA).add("Pai de aluno");
-        
-        lugares.get(CONCERTO_MUSICAL).add("Cantor");
-        lugares.get(CONCERTO_MUSICAL).add("Fã");
-        lugares.get(CONCERTO_MUSICAL).add("Empresário");
-        
-        lugares.get(PARQUE).add("Criança");
-        lugares.get(PARQUE).add("Policial");
-        lugares.get(PARQUE).add("Cachorro");
-
-    }
     
-    public boolean sortearLugar() {
-        if(jogadores.size() == 4){
-            Random rand = new Random();
-            int posicao = rand.nextInt(5);
-            IDlugar = posicao;
-
-            switch (posicao) {
-                case HOSPITAL:
-                    lugarDaPartida = "Hospital";
-                    break;
-                case RESTAURANTE:
-                    lugarDaPartida = "Restaurante";
-                    break;
-                case ESCOLA:
-                    lugarDaPartida = "Escola";
-                    break;
-                case CONCERTO_MUSICAL:
-                    lugarDaPartida = "Concerto Musical";
-                    break;
-                case PARQUE:
-                    lugarDaPartida = "Parque";
-                    break;
-            }
-            return true;
-        }
-        return false;
-    }
-    /*
     public void criarCartas() {
-        Random rand = new Random();
-        // cria as cartas 
-        for(int i = 0; i < 3; i++) {
-            Carta carta = new Carta();
-            cartas.add(carta);
-        }
-        // adiciona o lugar
-        cartas.forEach((c) -> {
-            c.setLugar(lugarDaPartida);
-        });
-        // adiciona as funcoes
-        cartas.forEach((c) -> {
-            int n = rand.nextInt(3); // corre o risco de todos receber a mesma funcao VEERIFICAR ISSO!!!!!!
-            c.setFuncao(lugares.get(IDlugar).get(n)); 
-        });
+    
         
     }
     
@@ -110,22 +40,31 @@ public class SpyFallAplication {
         // distribue as cartas para os jogadores
     }
     
-    public void sorteio() {
+    public void sorteios() {
         // sortear espiao
         Sort sorteio1 = new Sort(MAX_JOGADORES);
         sorteio1.start();
-            
-        
+        espiaoID = sorteio1.getValor();
         
         // sortear lugar
-        // sortear funcoes
+        Sort sorteio2 = new Sort(5);
+        sorteio2.start();
+        lugarID = sorteio2.getValor();
     }
     
     public static void setJogadores(Jogador jogador){
         jogadores.add(jogador);
         System.out.println(jogadores.size());
     }
-    */
+    
+    private void setLugares() {
+        lugares.add("Hospital");
+        lugares.add("Restaurante");
+        lugares.add("Escola");
+        lugares.add("Concerto Musical");
+        lugares.add("Praça");
+    }
+    
     
     /**
      * @param args the command line arguments
